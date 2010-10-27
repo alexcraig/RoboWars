615517ea512e05d05b2cfafda298f39223666c9b
Alexander Craig <gamefreakf1@gmail.com>
2010-10-27 12:14:49 -0400

    Added JUnit library fixed class path issues.

---
+++ src/robowars/test/ServerLobbyTest.java
@@ -0,0 +1,114 @@
+package robowars.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ServerLobbyTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testServerLobby() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddLobbyStateListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveLobbyStateListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterRobot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnregisterRobot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBroadcastMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBroadcastUserStateUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGameType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGameInProgress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentGameType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetServerName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaunchGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearCurrentGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testServerShutdown() {
+		fail("Not yet implemented");
+	}
+
+}
